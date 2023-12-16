59d4b20ccfcb941390010803c906c7a0c5b72384
Raed Ghamrawi <raed_ghamrawi@live.com>
2023-12-16 17:59:29 +0200

    continue crud

--- positions/views.py
+++ positions/views.py
@@ -9,22 +9,39 @@ def list_positions(request):
    context = { 'positions': positions}
    return render(request,'positions\positions_list.html', context)
 
-def positions(request):  
+def positions(request, id = 0):  
      
     if request.method == "POST":
-       
-        form = PositionForm(request.POST)
-        if form.is_valid():
-           
-            title= form.cleaned_data['title']
-            description = form.cleaned_data['description']
-            pos = Position.objects.create(title=title, description = description)
-            pos.save()
-            return redirect('list_positions')
-    else:   
-         form = PositionForm()
+       if id == 0: # to create a new record and append it to the table  
+            print("create new record")
+            form = PositionForm(request.POST)
+            if form.is_valid():
+                title= form.cleaned_data['title']
+                description = form.cleaned_data['description']
+                pos = Position.objects.create(title=title, description = description)
+                pos.save()
+                return redirect('list_positions')
+       else: # to update the edited record in the table
+            print("the update submitted")
+            position = Position.objects.get(pk=id)
+            form = PositionForm(request.POST, instance = position)  
+            if form.is_valid():
+               pos.save()
+               return redirect('list_positions')
+            
+    else:   # GET
+         if id == 0 : # to open a blank from
+            form = PositionForm()
+         else: # to populate the form with the data needed to be updated
+            position = Position.objects.get(pk=id)
+            form = PositionForm(instance=position)
+
          context = {
                     'form':form,
                }
          return render(request, "positions\positions.html", context)
+    
+
    
+def del_position(request, id = 0):
+       return redirect('list_positions') 
