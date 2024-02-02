29a9fe0245656c7e7478e76f635dcb087a06e486
raedGham <raed_ghamrawi@live.com>
2024-02-02 13:40:58 +0200

    vacation pdf

--- vacations/admin.py
+++ vacations/admin.py
@@ -1,16 +1,12 @@
 from django.contrib import admin
 from .models import Vacation, EmployeeLeaveStat
-from import_export.admin import ImportExportModelAdmin
+#from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 
 class VacAdmin(admin.ModelAdmin):
-    list_display = ('vac_date','employee','from_date','to_date','nodays','ampm')
+    list_display = ('vac_date','employee','from_date','to_date','nodays','ampm','first_approval', 'first_app_status','first_app_date')
 
 
+admin.site.register(Vacation, VacAdmin)
 admin.site.register(EmployeeLeaveStat)
-
-@admin.register(Vacation)
-
-class ViewAdmin(ImportExportModelAdmin):
-    pass
