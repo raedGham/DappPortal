4d17654850eb81e8cde88cda9e33777ce64ff441
Raed Ghamrawi <raed_ghamrawi@live.com>
2024-02-05 19:20:55 +0200

    htmx sucesss

--- overtime/forms.py
+++ overtime/forms.py
@@ -9,28 +9,27 @@ from django.contrib import messages
 
 class OvertimeForm(forms.ModelForm):
 
-  employee  = forms.ModelChoiceField(queryset=Account.objects.filter() )
+  # employee  = forms.ModelChoiceField(queryset=Account.objects.filter() )
   ot_date = forms.DateField(initial=datetime.today , widget = forms.DateInput(format="%Y-%m-%d", attrs={"type": "date"}))
   from_time = forms.TimeField(widget = forms.TimeInput(format='%H:%M', attrs={"type": "time"}))
   to_time   = forms.TimeField(widget = forms.TimeInput(format='%H:%M', attrs={"type": "time"}) ) 
   rate      = forms.ChoiceField(required=False, choices=(('','--'),('1.5','1.5'),('2.0','2.0')))
   reason   = forms.CharField(required=False,widget=forms.Textarea(attrs={'rows':'1'}))
+ 
   
-
-       
         
 
   class Meta:
     model = Overtime
-    fields = ['employee','ot_date','from_time', 'to_time','rate', 'reason']
+    fields = ['ot_date','from_time', 'to_time', 'reason']
     
   def __init__(self,*args, **kwargs):
-    dep_id = kwargs.pop('dep_id', None)
+   # dep_id = kwargs.pop('dep_id', None)
    
     super(OvertimeForm, self).__init__(*args, **kwargs)
     
-    if dep_id is not None:            
-            self.fields['employee'].queryset = Account.objects.filter(department=dep_id)
+    # if dep_id is not None:            
+    #         self.fields['employee'].queryset = Account.objects.filter(department=dep_id)
 
     for field in self.fields:
         self.fields[field].widget.attrs['class']= "form-control"
