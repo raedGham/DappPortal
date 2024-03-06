0b15d64b35cd4cfc09b7c0ae171d8c85e0536afb
raedGham <raed_ghamrawi@live.com>
2024-03-06 14:01:47 +0200

    correct profiles add

--- dapp/settings.py
+++ dapp/settings.py
@@ -89,19 +89,19 @@ WSGI_APPLICATION = 'dapp.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-
-     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'dappportal',
-        'USER': 'root',
-        'PASSWORD': 'Root',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+    #  'default': {
+    #     'ENGINE': 'django.db.backends.mysql', 
+    #     'NAME': 'dappportal',
+    #     'USER': 'root',
+    #     'PASSWORD': 'Root',
+    #     'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+    #     'PORT': '3306',
+    # }
 
 
 }
